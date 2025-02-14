273f6ee69fddc70ecb35d110179ec8f809c8412d
Ojasvi913 <ojasvi91329@gmail.com>
2025-02-14 18:42:18 +0530

    pid without sync

--- P2p test/test_without_sync.ino
+++ P2p test/test_without_sync.ino
@@ -1,67 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <Arduino.h>
-
 #define WHEEL_DIAMETER 4.4 // in cm
 #define ENCODER_COUNTS_PER_ROTATION 425
 #define SETPOINT_DISTANCE 26.0 // Target distance in cm
+// Encoder Pins
+#define M1_ENC_A PB3  // Encoder A pin for Motor 1
+#define M1_ENC_B PA15 // Encoder B pin for Motor 1
+#define M2_ENC_A PB9  // Encoder A pin for Motor 2
+#define M2_ENC_B PB8  // Encoder B pin for Motor 2
 
-#define M1_ENC_A 2 // Encoder A pin for Motor 1
-#define M1_ENC_B 3 // Encoder B pin for Motor 1
-#define M2_ENC_A 4 // Encoder A pin for Motor 2
-#define M2_ENC_B 5 // Encoder B pin for Motor 2
-
-#define M1_PWM 6  // Motor 1 PWM pin
-#define M1_IN1 7  // Motor 1 direction pin 1
-#define M1_IN2 8  // Motor 1 direction pin 2
-#define M2_PWM 9  // Motor 2 PWM pin
-#define M2_IN1 10 // Motor 2 direction pin 1
-#define M2_IN2 11 // Motor 2 direction pin 2
+// Motor Control Pins
+#define M1_PWM PA0   // Motor 1 PWM pin
+#define M1_IN1 PA2   // Motor 1 direction pin 1
+#define M1_IN2 PA3   // Motor 1 direction pin 2
+#define M2_PWM PA1   // Motor 2 PWM pin
+#define M2_IN1 PA4   // Motor 2 direction pin 1
+#define M2_IN2 PA5   // Motor 2 direction pin 2
 
 volatile int count_left = 0, count_right = 0;
 
 // Interrupt function for Motor 1 Encoder
-void IRAM_ATTR M1_Encoder_Interrupt() {
+void M1_Encoder_Interrupt() {
     if (digitalRead(M1_ENC_B) == HIGH) count_left++;  // Forward
     else count_left--;  // Reverse
 }
 
 // Interrupt function for Motor 2 Encoder
-void IRAM_ATTR M2_Encoder_Interrupt() {
-    if (digitalRead(M2_ENC_B) == HIGH) count_right++;  // Forward
+void M2_Encoder_Interrupt() {
+    if (digitalRead(M2_ENC_A) == HIGH) count_right++;  // Forward
     else count_right--;  // Reverse
 }
 
+// Function to control motor speed and direction
+void motorControl(int pwmPin, int in1, int in2, int speed) {
+    speed = constrain(speed, -255, 255); // Ensure speed is within valid range
 
-void motorControl(int pwmPin, int in1, int in2, int speed)
-{
-    speed = constrain(speed, -255, 255);
-    if (speed > 0)
-    {
+    if (speed > 0) {
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
-    }
-    else if (speed < 0)
-    {
+    } else if (speed < 0) {
         digitalWrite(in1, LOW);
         digitalWrite(in2, HIGH);
-        speed = -speed;
-    }
-    else
-    {
+        speed = -speed;  // Convert speed to positive for PWM
+    } else {
         digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW); // Stop the motor
+        digitalWrite(in2, LOW);
     }
     analogWrite(pwmPin, speed);
-}
-
-void brake()
-{
-    digitalWrite(M1_IN1, LOW);
-    digitalWrite(M1_IN2, LOW);
-    digitalWrite(M2_IN1, LOW);
-    digitalWrite(M2_IN2, LOW);
 }
 
 void pid_control()
@@ -90,7 +74,8 @@ void pid_control()
 
         if (abs(error_left) < 2 && abs(error_right) < 2)
         {
-            brake();
+            motorControl(M1_PWM, M1_IN1, M1_IN2, 0);
+            motorControl(M2_PWM, M2_IN1, M2_IN2, 0);
             break;
         }
 
@@ -103,18 +88,18 @@ void pid_control()
         motorControl(M1_PWM, M1_IN1, M1_IN2, control_left);
         motorControl(M2_PWM, M2_IN1, M2_IN2, control_right);
 
-        usleep(5000); // Small delay to stabilize PID loop
     }
 }
+
 void setup() {
-    Serial.begin(9600);
-    
-    // Set encoder pins as input
+    Serial.begin(115200);
+    delay(3000);
+    // Set encoder pins as input pull-up
     pinMode(M1_ENC_A, INPUT_PULLUP);
     pinMode(M1_ENC_B, INPUT_PULLUP);
     pinMode(M2_ENC_A, INPUT_PULLUP);
     pinMode(M2_ENC_B, INPUT_PULLUP);
-
+    
     // Set motor control pins as output
     pinMode(M1_PWM, OUTPUT);
     pinMode(M1_IN1, OUTPUT);
@@ -125,13 +110,22 @@ void setup() {
 
     // Attach interrupts for encoders
     attachInterrupt(digitalPinToInterrupt(M1_ENC_A), M1_Encoder_Interrupt, RISING);
-    attachInterrupt(digitalPinToInterrupt(M2_ENC_A), M2_Encoder_Interrupt, RISING);
+    attachInterrupt(digitalPinToInterrupt(M2_ENC_B), M2_Encoder_Interrupt, RISING);
 }
 
-int main()
-{
+void loop() {
+    // Set motor speed for testing
+    // int test_speed = 150;
+    
+    // motorControl(M1_PWM, M1_IN1, M1_IN2, test_speed);
+    // motorControl(M2_PWM, M2_IN1, M2_IN2, test_speed);
+
+    // Serial.print("Left Encoder: ");
+    // Serial.print(count_left);
+    // Serial.print(" | Right Encoder: ");
+    // Serial.println(count_right);
+
+    // delay(100);  // 100ms delay
 
     pid_control();
-
-    return 0;
 }
